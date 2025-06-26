fe2a9ef94585201fc343fdb87a038d152e5e00d3
HS <mcm009@naver.com>
2025-06-26 17:46:36 +0900

    fix : 충돌 에러 해결

--- backend/types/types.ts
+++ backend/types/types.ts
@@ -1,3 +1,5 @@
+import { Request } from 'express';
+
 export interface IUser {
     id : number,
     email : string,
@@ -14,4 +16,25 @@ export interface post {
     is_health?:string,
     state?:number,
     plantTag?: string[]
+}
+
+export interface IPlant {
+
+}
+
+export interface IComment {
+    id: number;
+    userId: number;
+    postId: number;
+    content: string;
+    createdAt: Date;
+}
+
+export interface ICommnetWithUser extends IComment {
+    nickName: string;
+    isAuthor: boolean;
+}
+
+export interface TypedRequest<T> extends Request {
+    body: T;
 }
