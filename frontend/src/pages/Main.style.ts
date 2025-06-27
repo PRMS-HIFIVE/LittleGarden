db66cb4808b2bac3656aef6c1a535c3cbbf33447
1dg <ricky2425@naver.com>
2025-06-27 12:30:18 +0900

    feat: 임시 commit

---
+++ frontend/src/pages/Main.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const MainWrap = styled.div`
+    width: 360px;
+
+    margin: 0 auto;
+`
---
+++ frontend/src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import * as S from './Main.style'
+
+function Main(){
+    return(
+        <S.MainWrap>
+            <h1>Main</h1>
+        </S.MainWrap>
+    )
+}
+
+export default Main;
