f8d3523585fff5e3f364d6b0995f0912afbeeabd
airbus <001airbus@gmail.com>
2025-07-01 13:02:40 +0900

    feat: 폼 기본구성 (#7)

--- frontend/src/common/From/DiaryForm/DiaryFrom.tsx
+++ frontend/src/common/From/DiaryForm/DiaryFrom.tsx
@@ -1,4 +1,5 @@
 import { DiaryContainer, DiaryHeaderTitle, DiaryStyledForm, DiaryStyledTextarea } from "@/common/From/DiaryForm/DiaryForm.styles";
+import ImageUploadBox from "@/common/From/DiaryForm/ImageUploadBox/ImageUploadBox";
 import Button from "@/components/UI/Button/Button";
 import Input from "@/components/UI/Input/Input";
 
@@ -17,6 +18,9 @@ const DiaryForm =({
                 <Input placeholder="제목을 입력해주세요" radius="8px"/>
                 <DiaryStyledTextarea></DiaryStyledTextarea>
                 <Input placeholder="태그" radius="8px"/>
+
+                <ImageUploadBox />
+
                 <Button color="navyBlue" type="submit">올리기</Button>
             </DiaryStyledForm>
         </DiaryContainer>
