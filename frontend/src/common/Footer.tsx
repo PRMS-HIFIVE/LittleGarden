1715c405ce0e1593231b2f2e9212f9aa7c1597d0
ricky0601 <ricky2425@naver.com>
2025-07-03 21:00:28 +0900

    feat: 카메라와 로고 버전 Footer 수정

--- frontend/src/common/Footer.tsx
+++ frontend/src/common/Footer.tsx
@@ -2,9 +2,21 @@ import * as S from './Footer.style';
 import Camera from '../assets/icons/camera.svg?react';
 import type { ChangeEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { IconArrowLeft, IconLogo } from '@/assets/icons/IconList';
 
-function Footer () {
+export interface FooterProps{
+    type?: 'camera' | 'post';
+}
+
+const Footer = ({
+        type = 'camera'
+    }: FooterProps) => {
+    const footerType = type ?? 'camera';
     const navigate = useNavigate();
+
+    const handlePage = () => {
+        navigate(-1);
+    }
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
@@ -16,17 +28,37 @@ function Footer () {
 
     return(
         <S.FooterWrapper>
-            <label htmlFor="camera-upload" style={{ cursor: 'pointer' }}>
-                <Camera />
-            </label>
-            <input
-                id="camera-upload"
-                style={{ display: 'none' }}
-                accept="image/*"
-                type="file"
-                capture="environment"
-                onChange={handleFileChange}
-            />
+            {
+                footerType === 'camera' && (
+                    <>
+                        <IconArrowLeft
+                            onClick={handlePage}
+                            className='back-arrow' />
+                        <label htmlFor="camera-upload" style={{ cursor: 'pointer' }}>
+                            <Camera />
+                        </label>
+                        <input
+                            id="camera-upload"
+                            style={{ display: 'none' }}
+                            accept="image/*"
+                            type="file"
+                            capture="environment"
+                            onChange={handleFileChange}
+                        />
+                    </>
+                )
+            }
+            {
+                footerType === 'post' && (
+                    <>
+                        <IconArrowLeft
+                            onClick={handlePage}
+                            className='back-arrow' />
+                        <IconLogo size={32} />
+                    </>
+                )
+
+            }
         </S.FooterWrapper>
     )
 }
