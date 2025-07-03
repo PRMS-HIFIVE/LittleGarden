3bb18229757b536c35b62035d331026a0f908b65
ricky0601 <ricky2425@naver.com>
2025-07-03 20:59:51 +0900

    feat: Logo List에 추가

--- frontend/src/assets/icons/IconList.ts
+++ frontend/src/assets/icons/IconList.ts
@@ -4,6 +4,7 @@ import Menu from "@/assets/icons/Menu.svg?react";
 import ChevronLeft from "@/assets/icons/ChevronLeft.svg?react";
 import Star from "@/assets/icons/Star.svg?react";
 import Edit from "@/assets/icons/Edit.svg?react";
+import Logo from "@/assets/icons/Logo.svg?react"
 
 export const IconBell = styled(Bell)<{size?: number}>`
     width: ${({size}) => size ? `${size}px` : '24px'};
@@ -37,6 +38,12 @@ export const IconStar = styled(Star)<{size?: number}>`
 `;
 
 export const IconEdit = styled(Edit)<{size?: number}>`
+    width: ${({size}) => size ? `${size}px` : '24px'};
+    height: ${({size}) => size ? `${size}px` : '24px'};
+    cursor: pointer;
+`
+
+export const IconLogo = styled(Logo)<{size?: number}>`
     width: ${({size}) => size ? `${size}px` : '24px'};
     height: ${({size}) => size ? `${size}px` : '24px'};
     cursor: pointer;
