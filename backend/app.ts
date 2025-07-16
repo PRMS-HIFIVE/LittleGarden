69786d066dc2dda992e5dda075b8ce38761bd1ed
j1w0os <102014705+sonjiwoo1215@users.noreply.github.com>
2025-07-16 17:59:32 +0900

    fix: 404에러

--- backend/app.ts
+++ backend/app.ts
@@ -3,12 +3,12 @@ import cors from 'cors';
 import { StatusCodes } from 'http-status-codes';
 import http from 'http';
 import path from "path";
-
 import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
 
 app.use(cors({
     origin: process.env.FRONT_SERVER_URL || "http://localhost:5173",
@@ -34,13 +34,10 @@ app.use("/push", pushRouter);
 // 스케줄링
 import './service/scheduleService';
 
-// 정적 파일 제공: frontend/dist 폴더 안의 빌드 결과물 서빙
+// 정적 파일 서빙
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// SPA 라우팅 처리: 모든 미등록 경로에 대해 index.html 반환
-app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+
 
 // catch 404 and forward to error handler
 // app.use(function(req : Request, res : Response) {
@@ -51,4 +48,20 @@ const server = http.createServer(app);
 
 server.listen(PORT, () => {
     console.log(`💡 서버 포트: ${PORT}`);
+});
+
+const apiPaths = ['users', 'posts', 'plantidapi', 'plants', 'comments', 'push'];
+
+const apiRegexPart = apiPaths.join('|'); // "users|posts|plantidapi|plants|comments|push"
+
+// API 경로를 제외한 나머지 경로에 대해 SPA index.html 반환
+app.get(new RegExp(`^\/(?!(${apiRegexPart})).*`), (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"), err => {
+    if (err) {
+      console.error("index.html 전송 실패:", err);
+      if (!res.headersSent) {
+        res.status((err as any).status || 500).end();
+      }
+    }
+  });
 });
