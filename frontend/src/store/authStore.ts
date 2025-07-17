dcd36e208555ac861816b30e55fe95ae6e6098b1
1dg <ricky2425@naver.com>
2025-07-17 09:48:41 +0900

    feat: localStorage에서 cookie로 변경

--- frontend/src/store/authStore.ts
+++ frontend/src/store/authStore.ts
@@ -1,5 +1,20 @@
 import { create } from "zustand";
 
+const getCookie = (name: string): string | null => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) {
+    const cookieValue = parts.pop()?.split(";").shift();
+    // 쿠키 값이 없는 경우 null을 반환하도록 수정
+    return cookieValue ? decodeURIComponent(cookieValue) : null;
+  }
+  return null;
+};
+
+const eraseCookie = (name: string) => {
+  document.cookie = `${name}=; Max-Age=-99999999; path=/`;
+};
+
 interface User {
   id: number;
   nickname: string;
@@ -19,7 +34,7 @@ interface AuthState {
 }
 
 export const useAuthStore = create<AuthState>((set) => {
-  const token = localStorage.getItem("token");
+  const token = getCookie("access_token");
   const userRaw = localStorage.getItem("user");
   const user = userRaw ? JSON.parse(userRaw) : null;
 
@@ -32,8 +47,12 @@ export const useAuthStore = create<AuthState>((set) => {
     setEmail: (email) => set({ email }),
     setUserId: (userId) => set({ userId }),
     setToken: (token) => {
-      localStorage.setItem("token", token ?? "");
-      set({ token });
+      if (token) {
+        document.cookie = `access_token=${encodeURIComponent(token)}; path=/`;
+      } else {
+        eraseCookie("access_token");
+      }
+      set({ token: token });
     },
     setUser: (user) => {
       if (user) {
@@ -44,8 +63,8 @@ export const useAuthStore = create<AuthState>((set) => {
       set({ user });
     },
     resetAuth: () => {
-      localStorage.removeItem("token");
       localStorage.removeItem("user");
+      eraseCookie("access_token");
       set({
         email: "",
         userId: null,
